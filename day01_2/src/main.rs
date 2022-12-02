6b0783082eb5483a3049fd1b43ed19e70bd2890e
panicbit <panicbit.dev@gmail.com>
2022-12-02 06:29:54 +0100

    day 1 part 2

--- day01_2/src/main.rs
+++ day01_2/src/main.rs
@@ -1,6 +1,21 @@
-use aoc::aoc;
+use std::cmp::Reverse;
 
-#[aoc(2022, 01, 2)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+use aoc::aoc;
+use itertools::Itertools;
+
+#[aoc(2022, 1, 2)]
+fn main(input: &str) -> u32 {
+    let calories = input
+        .split("\n\n")
+        .map(|inventory| {
+            inventory
+                .split_whitespace()
+                .map(|calories| calories.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .sorted_by_key(|calories| Reverse(*calories))
+        .take(3)
+        .sum();
+
+    calories
 }
