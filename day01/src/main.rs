5873540069f73656ccd58b872f55b02fd87cacf7
panicbit <panicbit.dev@gmail.com>
2022-12-02 06:24:42 +0100

    day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,6 +1,17 @@
 use aoc::aoc;
 
-#[aoc(2022, 01, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+#[aoc(2022, 1, 1)]
+fn main(input: &str) -> u32 {
+    let calories = input
+        .split("\n\n")
+        .map(|inventory| {
+            inventory
+                .split_whitespace()
+                .map(|calories| calories.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .max()
+        .unwrap();
+
+    calories
 }
