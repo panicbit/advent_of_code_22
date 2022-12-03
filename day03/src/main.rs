6024f91a8c5856b3c4303ac9447f0cdc0bb156de
panicbit <panicbit.dev@gmail.com>
2022-12-03 15:30:53 +0100

    day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,28 @@
+use std::collections::HashSet;
+
 use aoc::aoc;
 
-#[aoc(2022, 03, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+#[aoc(2022, 3, 1)]
+fn main(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|sack| {
+            let mid = sack.len() / 2;
+            let (comp1, comp2) = sack.split_at(mid);
+            let comp1 = comp1.chars().collect::<HashSet<_>>();
+            let comp2 = comp2.chars().collect::<HashSet<_>>();
+            let mut intersection = comp1.intersection(&comp2);
+            let common_item = *intersection.next().unwrap();
+
+            priority(common_item)
+        })
+        .sum()
+}
+
+fn priority(item: char) -> u32 {
+    match item {
+        'a'..='z' => item as u32 - 'a' as u32 + 1,
+        'A'..='Z' => item as u32 - 'A' as u32 + 27,
+        _ => unreachable!(),
+    }
 }
