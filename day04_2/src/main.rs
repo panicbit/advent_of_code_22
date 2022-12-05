f0049f8e66b41234005c0d06f969579e31cb43bf
panicbit <panicbit.dev@gmail.com>
2022-12-05 02:30:17 +0100

    day 4 part 2

--- day04_2/src/main.rs
+++ day04_2/src/main.rs
@@ -1,6 +1,43 @@
 use aoc::aoc;
 
-#[aoc(2022, 04, 2)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+#[aoc(2022, 4, 2)]
+fn main(input: &str) -> usize {
+    input
+        .lines()
+        .map(|pair| {
+            let (range1, range2) = pair.split_once(',').unwrap();
+            let range1 = Range::parse(range1);
+            let range2 = Range::parse(range2);
+
+            (range1, range2)
+        })
+        .filter(|(range1, range2)| range1.overlaps_with(range2))
+        .count()
+}
+
+struct Range {
+    start: u32,
+    end: u32,
+}
+
+impl Range {
+    fn parse(s: &str) -> Self {
+        let (start, end) = s.split_once('-').unwrap();
+
+        Self {
+            start: start.parse::<u32>().unwrap(),
+            end: end.parse::<u32>().unwrap(),
+        }
+    }
+
+    fn contains(&self, value: u32) -> bool {
+        self.start <= value && value <= self.end
+    }
+
+    fn overlaps_with(&self, other: &Self) -> bool {
+        self.contains(other.start)
+            || self.contains(other.end)
+            || other.contains(self.start)
+            || other.contains(self.end)
+    }
 }
