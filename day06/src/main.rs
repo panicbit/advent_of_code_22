03d6c37bf5375afa0829110b5d215ba135505d19
panicbit <panicbit.dev@gmail.com>
2022-12-06 09:13:38 +0100

    day 6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,13 @@
-use aoc::aoc;
+use std::collections::HashSet;
 
-#[aoc(2022, 06, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+use aoc::aoc;
+use itertools::Itertools;
+
+#[aoc(2022, 6, 1)]
+fn main(input: &str) -> usize {
+    input
+        .chars()
+        .tuple_windows()
+        .position(|(a, b, c, d)| HashSet::from([a, b, c, d]).len() == 4)
+        .unwrap() + 4
 }
